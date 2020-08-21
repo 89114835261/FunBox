b4e498af36dec5f546d3e0d6768b8c9a90ec1f71
Denis_Bespalov <googlonator@gmail.com>
2020-08-21 03:15:48 +0300

    Добавили библиотеку react-helmet для метатегов, что бы запретить маштабирование на смартфонаx

--- src/Component/Main/Main.js
+++ src/Component/Main/Main.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import style from './Main.module.scss';
 import ProductCard from '../ProductCard/ProductCard';
+import { Helmet } from 'react-helmet';
 
 const Main = (props) => {
     return(
         <div className={style.app}>
+            <Helmet>
+                <meta name="viewport" content="width=device-width, user-scalable=no" />
+            </Helmet>
             <div className={style.shadow}></div>
             <div className={style.content}>
                 <h2 className={style.pageTitle}>{props.title}</h2>
